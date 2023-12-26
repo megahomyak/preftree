fd87833fe659420b6d91bd012ff3c108de6f14d2
megahomyak <g.megahomyak@gmail.com>
2023-12-26 05:29:25 +0500

    Certainly, did some work

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,8 @@ impl<K: Hash + Eq, V> PrefixTree<K, V> {
         }
     }
 
-    /// Returns the previous value at the same key, if there was one
+    /// Inserts the specified value at the specified key; returns the previous value at the same
+    /// key, if there was one
     pub fn insert(&mut self, sequence: impl Iterator<Item = K>, value: V) -> Option<V> {
         let mut root = self;
         for item in sequence {
@@ -26,58 +27,76 @@ impl<K: Hash + Eq, V> PrefixTree<K, V> {
         root.value.replace(value)
     }
 
-    /// Returns a reference to the value associated with the shortest prefix of the given sequence
-    /// (or `None` if no prefixes were found)
-    pub fn get_by_shortest_prefix<'a>(&self, mut sequence: impl Iterator<Item = &'a K>) -> Option<&V> {
+    /// Returns an immutable reference to the value associated with the shortest prefix of the
+    /// given sequence (or `None` if no prefixes were found)
+    pub fn get_by_shortest_prefix<I: AsRef<K>>(
+        &self,
+        mut sequence: impl Iterator<Item = I>,
+    ) -> Option<&V> {
         let mut root = self;
         loop {
             if matches!(root.value, Some(_)) {
                 return (&root.value).into();
             }
-            let Some(new_root) = sequence.next().and_then(|item| root.subtrees.get(item)) else {
-                return None;
+            root = match sequence
+                .next()
+                .and_then(|item| root.subtrees.get(item.as_ref()))
+            {
+                Some(subtree) => subtree,
+                None => return None,
             };
-            root = new_root;
         }
     }
 
     /// Returns a mutable reference to the value associated with the shortest prefix of the given
     /// sequence (or `None` if no prefixes were found)
-    pub fn get_by_shortest_prefix_mut<'a>(&mut self, mut sequence: impl Iterator<Item = &'a K> + 'a) -> Option<&mut V> {
+    pub fn get_by_shortest_prefix_mut<I: AsRef<K>>(
+        &mut self,
+        mut sequence: impl Iterator<Item = I>,
+    ) -> Option<&mut V> {
         let mut root = self;
         loop {
             if matches!(root.value, Some(_)) {
                 return (&mut root.value).into();
             }
-            let Some(new_root) = sequence.next().and_then(|item| root.subtrees.get_mut(item)) else {
-                return None;
+            root = match sequence
+                .next()
+                .and_then(|item| root.subtrees.get_mut(item.as_ref()))
+            {
+                Some(subtree) => subtree,
+                None => return None,
             };
-            root = new_root;
         }
     }
 
     /// Returns a mutable reference to the value associated with the exact match of the given
     /// sequence (or `None` if no such sequence is found)
-    pub fn get_exact_match_mut<'a>(&mut self, mut sequence: impl Iterator<Item = &'a K>) -> Option<&mut V> {
+    pub fn get_exact_match_mut<I: AsRef<K>>(
+        &mut self,
+        sequence: impl Iterator<Item = I>,
+    ) -> Option<&mut V> {
         let mut root = self;
         for item in sequence {
-            let Some(new_root) = root.subtrees.get_mut(item) {}
-        }
-        &mut root.value
-        loop {
-            if matches!(root.value, Some(_)) {
-                return (&mut root.value).into();
+            root = match root.subtrees.get_mut(item.as_ref()) {
+                Some(subtree) => subtree,
+                None => return None,
             }
-            let Some(new_root) = sequence.next().and_then(|item| root.subtrees.get_mut(&item)) else {
-                return None;
-            };
-            root = new_root;
         }
+        (&mut root.value).into()
     }
-}
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+    /// Returns an immutable mutable reference to the value associated with the exact match of the
+    /// given sequence (or `None` if no such sequence is found)
+    pub fn get_exact_match<I: AsRef<K>>(&self, sequence: impl Iterator<Item = I>) -> Option<&V> {
+        let mut root = self;
+        for item in sequence {
+            root = match root.subtrees.get(item.as_ref()) {
+                Some(subtree) => subtree,
+                None => return None,
+            }
+        }
+        (&root.value).into()
+    }
 }
 
 #[cfg(test)]
@@ -85,8 +104,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    fn it_works() {}
 }
