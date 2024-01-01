202dc9da726d6dedc10246bb7da91d6d9f952577
megahomyak <g.megahomyak@gmail.com>
2024-01-01 21:31:34 +0500

    Degeneralization

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ pub struct PrefixTree<K, V> {
     subtrees: HashMap<K, PrefixTree<K, V>>,
 }
 
+pub struct Iter<K, V> {
+
+}
+
 impl<K: Hash + Eq, V> PrefixTree<K, V> {
     pub fn new() -> Self {
         Self {
@@ -98,17 +102,6 @@ impl<K: Hash + Eq, V> PrefixTree<K, V> {
         (&root.value).into()
     }
 
-    /// Removes empty subtrees from the tree to decrease memory consumption
-    ///
-    /// Meaning of arguments:
-    /// * `last_root` - the farthest root from the base root. Should have its `value` set to
-    ///   `None`, otherwise there will be information loss
-    /// * `roots` - all the other roots, from the base one to the one that is before the
-    ///   `last_root`, with keys that were used with them to retrieve the next root
-    fn remove_empty_subtrees<I: AsRef<K>>(&self, last_root: &mut Self, roots: Vec<(*mut Self, I)>) {
-
-    }
-
     /// Removes the value associated with the exact match of the given sequence from the tree and
     /// returns it (or returns `None` if no matching value was found)
     pub fn remove_exact_match<I: AsRef<K>>(
@@ -149,36 +142,52 @@ impl<K: Hash + Eq, V> PrefixTree<K, V> {
     /// and returns it (or returns `None` if no matching value was found)
     pub fn remove_by_shortest_prefix<I: AsRef<K>>(
         &mut self,
-        sequence: impl Iterator<Item = I>,
+        mut sequence: impl Iterator<Item = I>,
     ) -> Option<V> {
         let mut root = self;
         let mut keys = Vec::new();
         loop {
-            if root
-        }
-        for item in sequence {
-            let key = root as *mut _;
-            root = match root.subtrees.get_mut(item.as_ref()) {
-                Some(subtree) => subtree,
+            if matches!(root.value, Some(_)) {
+                break;
+            }
+            let old_root = root as *mut _;
+            let item;
+            (root, item) = match sequence.next().and_then(|item| {
+                root.subtrees
+                    .get_mut(item.as_ref())
+                    .map(|subtree| (subtree, item))
+            }) {
+                Some((subtree, item)) => (subtree, item),
                 None => return None,
             };
-            keys.push((key, item));
+            keys.push((old_root, item));
         }
         let result = root.value.take();
-        let mut keys = keys.into_iter().rev();
+        let mut roots = keys.into_iter().rev();
         let mut root: *mut _ = root;
         loop {
             if !unsafe { (*root).subtrees.is_empty() } {
                 break;
             }
-            let key;
-            (root, key) = match keys.next() {
-                Some((root, key)) => (root, key),
+            let item;
+            (root, item) = match roots.next() {
+                Some((root, item)) => (root, item),
                 None => break,
             };
-            unsafe { (*root).subtrees.remove(key.as_ref()) };
+            unsafe { (*root).subtrees.remove(item.as_ref()) };
+            if unsafe { (*root).value.is_some() } {
+                break;
+            }
         }
         result
+    }
+
+    pub fn iter(&self) -> impl Iterator<>
+
+    pub fn iter(&self) -> impl Iterator<Item = (Vec<&K>, &V)> {
+        for key in self.subtrees.iter() {
+
+        }
     }
 }
 
@@ -187,7 +196,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-
+    fn deletion() {
+        let tree = PrefixTree::new();
     }
 }
